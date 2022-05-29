a3c08c7370603573a8d0b1a5783ab0ee2256ad1b
Matt Bauer <bauer.ms@pg.com>
2022-05-29 12:12:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,10 @@ if uploaded_file is not None:
     ax=ax,
   )
   st.write(fig)
+  
+st.download_button('Download Transformed CSV', df, 'text/csv')
+
+
 # with st.echo(code_location='below'):
 #     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 #     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
